b5aac33d9e6f8f7e4eb8bc7a39553e03c3ef26e2
Jen Vitrano <vitranojen@gmail.com>
2016-05-24 18:54:21 -0400

    Remove footer h3

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <!-- BEGIN FOOTER-->
       <footer class="row">
             <div class="twelve columns">
-                <h3>This is the Footer</h3>
                 <p>This is some cool footer content</p>
             </div> <!-- END CONTAINER DIV -->
       </footer>
