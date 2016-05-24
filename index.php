ef2654e947b38ca93dd31167d50228c1e111c3d3
Jen Vitrano <vitranojen@gmail.com>
2016-05-24 18:52:09 -0400

    Add page PHP content

--- index.php
+++ index.php
@@ -11,11 +11,12 @@
                   if ( have_posts() ) {
                     while ( have_posts() ) {
                       the_post(); ?>
-                      
+
                       <h3><?php the_title(); ?></h3>
                       <?php the_excerpt(); ?>
 
-                  <?php  } // end while
+                  <?php
+                    } // end while
                   } // end if
                 ?>
                 <!-- END LOOP -->
