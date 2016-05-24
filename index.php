9013c8f635c5db0d5e9e716233a2868bbe86c862
Jen Vitrano <vitranojen@gmail.com>
2016-05-24 17:53:16 -0400

    Comment index page

--- index.php
+++ index.php
@@ -10,26 +10,33 @@
 </head>
 <body>
     <div class="container">
+    <!-- BEGIN HEADER -->
         <header class="row">
             <div class="twelve columns">
                 <h1>Jen Vitrano Blank WordPress Template</h1>
                 <p>This is my WordPress template.</p>
             </div>
         </header>
+    <!-- END HEADER -->
 
+    <!-- BEGIN SECTION CONTAINER -->
         <section class="row">
             <div class="twelve columns">
                 <h2>This is the Section</h2>
                 <p>This is some cool section content</p>
             </div>
         </section>
+    <!-- END SECTION CONTAINER -->
 
+
+    <!-- BEGIN FOOTER-->
         <footer class="row">
               <div class="twelve columns">
                   <h2>This is the Footer</h2>
                   <p>This is some cool footer content</p>
               </div>
         </footer>
+    <!-- END FOOTER -->
     </div>
 
 </body>
