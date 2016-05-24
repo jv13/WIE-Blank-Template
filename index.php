51880043cd8ccc492f561fddcdf05a7baf5a9ebe
Jen Vitrano <vitranojen@gmail.com>
2016-05-24 18:36:00 -0400

    Add PHP for title and content of index page

--- index.php
+++ index.php
@@ -10,8 +10,12 @@
                 <?php
                   if ( have_posts() ) {
                     while ( have_posts() ) {
-                      the_post();
-                    } // end while
+                      the_post(); ?>
+                      
+                      <h3><?php the_title(); ?></h3>
+                      <?php the_excerpt(); ?>
+
+                  <?php  } // end while
                   } // end if
                 ?>
                 <!-- END LOOP -->
