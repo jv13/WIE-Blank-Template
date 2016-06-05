a5463c0a940694d73da4fb4034d9d32dd97f0735
Jen Vitrano <vitranojen@gmail.com>
2016-06-05 13:29:37 -0400

    Add images and thumbnails

--- functions.php
+++ functions.php
@@ -54,4 +54,7 @@ add_action('widgets_init', 'blank_widgets_init');
 
 add_theme_support('menus');
 
+/*--- Enable Post Thumbnails ---*/
+add_theme_support( 'post-thumbnails' );
+
 ?>
--- single.php
+++ single.php
@@ -6,12 +6,21 @@
             <?php if ( have_posts() ) {
                 while( have_posts() ) {
                     /* OUR DATA CONTEXT IS DEFINED */
-                    the_post(); ?>
+                    the_post();
+
+                    if ( has_post_thumbnail() ) { ?>
+                      <div class="post-thumbnail">
+                        <?php the_post_thumbnail('large'); ?>
+                      </div>
+                    <?php } ?>
 
                     <h2><?php the_title(); ?></h2>
-                    <?php the_content();
-                }
-            } ?>
+                    <?php the_content(); ?>
+
+                    <?php
+                  } // end while
+                } // end iff
+            ?>
 <!-- END PAGE PHP -->
 
         </div>
