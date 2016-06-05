9b1063cd275f24953f222a3513046e0f37f2e0fb
Jen Vitrano <vitranojen@gmail.com>
2016-06-05 13:39:51 -0400

    Add permalinks

--- index.php
+++ index.php
@@ -18,8 +18,10 @@
                         }
                       ?>
 
-                      <h3><?php the_title(); ?></h3>
+                      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                       <?php the_excerpt(); ?>
+
+                      <p><a href="<?php the_permalink(); ?>">Read More</a></p>
 
                   <?php
                     } // end while
